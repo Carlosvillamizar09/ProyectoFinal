6ce83df600103f1d41cc776352dc6ed24d7e9b90
Carlosvillamizar09 <carlosvilla0903@gmail.com>
2025-06-24 06:46:33 -0500

    feat: :sparkles: Se añade la funcion mostrar productos
    Con esta, se creara un div por cada producto el cual contendra toda su informacion

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ const cantidadModal = document.getElementById('cantidad-modal');
 const cantidadInput = document.getElementById('cantidad-input');
 const confirmarCantidad = document.getElementById('confirmar-cantidad');
 const cancelarCantidad = document.getElementById('cancelar-cantidad');
+
+function renderizarProductos(lista) {
+  contenedorProductos.innerHTML = '';
+  lista.forEach(producto => {
+    const div = document.createElement('div');
+    div.className = 'producto';
+    div.innerHTML = `
+      <img src="${producto.image}" alt="${producto.title}" />
+      <h4>${producto.title}</h4>
+      <p>$${producto.price}</p>
+      <button onclick="mostrarDescripcion(${producto.id})">Ver Descripción</button>
+      <button onclick="elegirCantidad(${producto.id})">Agregar al carrito</button>
+    `;
+    contenedorProductos.appendChild(div);
+  });
+}
